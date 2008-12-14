765c54f006247f2cd1612c0e87e222563f66ef82
rswolff <rswolff@6ac115d0-849f-4716-98aa-3b735b298374>
2008-12-14 03:35:04 +0000

    views now only retrieve calendar_events in the future; changed nav on application layout, other formatting on views show
    git-svn-id: http://www.tuscan.ca/svn/cws_calendar/trunk@11 6ac115d0-849f-4716-98aa-3b735b298374

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,6 +1,6 @@
 class View < ActiveRecord::Base
 
   has_many :calendar_event_views
-  has_many :calendar_events, :through => :calendar_event_views
+  has_many :calendar_events, :through => :calendar_event_views, :order => 'start_date, start_time', :conditions => ['start_date >= ?', Date.today ]
 
 end
