90d1a97196ad3a843c1e7f3cf272c1c432426ad6
Scott Wolff <scott.wolff@tuscan.ca>
2009-11-29 06:33:44 -0700

    added new date format

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,6 +1,7 @@
 ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
   :short => "%Y-%m-%d",
   :med => "%d-%b-%Y",
+  :med_with_day => "%d-%b-%Y (%a)",
   :long => "%A %B %d, %Y"
 )
  
